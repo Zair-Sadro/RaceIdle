c2de76384c5fd0718011ad3b06f6655c8f96aac9
Alex <alksgud@gmail.com>
2024-05-26 17:39:31 +0700

    BackGroundMusic

--- Assets/_Scripts/Systems/AudioService.cs
+++ Assets/_Scripts/Systems/AudioService.cs
@@ -1,4 +1,6 @@
 
+using System;
+using System.Collections;
 using UnityEngine;
 
 
@@ -6,9 +8,73 @@ public class AudioService : MonoBehaviour
 {
     [SerializeField] private AudioYB _audioPlayer1;
     [SerializeField] private AudioYB _audioPlayer2;
+    [SerializeField] private AudioYB _musicPlayer;
+    [SerializeField] private float _backGroundMusicVolume = 0.4f;
+    [SerializeField] private float _speedOfVolumeChange = 0.4f;
+
+    private const string BACK1 = "BACK1";
+    private const string BACK2 = "BACK2";
+    private const string BACK3 = "BACK3";
+
+    private void Start()
+    {
+        _musicPlayer.volume = 0;
+       StartCoroutine(LoopedBackgroundMusic());
+    }
+
+    IEnumerator LoopedBackgroundMusic()
+    {
+        var wait55sec = new WaitForSeconds(55f);
+        
+        _musicPlayer.Play(BACK3);
+        
+        while (true)
+        {
+            while (_musicPlayer.volume < _backGroundMusicVolume)
+            {
+                _musicPlayer.volume += _speedOfVolumeChange * Time.deltaTime;
+                yield return null;
+            }
+
+            yield return wait55sec;
+        
+            while (_musicPlayer.volume > 0.05f)
+            {
+                _musicPlayer.volume -= _speedOfVolumeChange * Time.deltaTime;
+                yield return null;
+            }
+            
+            _musicPlayer.Play(NextBackGroundMusic());
+            yield return null;
+        }
+        
+    }
+
+    private byte _indxOfBackMusic;
+
+    private string NextBackGroundMusic()
+    {
+        switch (_indxOfBackMusic)
+        {
+            case 0:
+                _indxOfBackMusic = 1;
+                return BACK1;
+            
+            case 1:
+                _indxOfBackMusic = 2;
+                return BACK2;
+
+            case 2:
+                _indxOfBackMusic = 0;
+                return BACK3;
+
+        }
+        return BACK1;
+    }
+
     public void PlayAudo(AudioName audioName) 
     {
-        if(_audioPlayer1.isPlaying) 
+        if(_audioPlayer1.isPlaying)
         {
             _audioPlayer2.Play(audioName.ToString(), true);
         }
@@ -30,5 +96,8 @@ public enum AudioName
     SHOP,
     CAR,
     SKID,
-    TILE
+    TILE,
+    BACK1,
+    BACK2,
+    BACK3
 }
